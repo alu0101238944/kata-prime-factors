521c4fa945036599ba35cc46618823692b9a7ff9
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-04-11 15:47:33 +0100

    Green: Product of prime factors of a number matches the number

--- prime_factors_test.py
+++ prime_factors_test.py
@@ -1,5 +1,6 @@
 
 import unittest
+import numpy
 from src.prime_factors import PrimeFactors
 
 class FizzBuzzTests(unittest.TestCase):
@@ -15,8 +16,13 @@ class FizzBuzzTests(unittest.TestCase):
   def test_three_has_only_itself_as_prime_factor(self):
     self.assertEqual(self.prime_factors.factorize(3), [3])
 
-  def test_four_has_two_twice_as_prime_factor(self):
+  def test_four_has_two_twice_as_prime_factors(self):
     self.assertEqual(self.prime_factors.factorize(4), [2, 2])
+
+  def test_product_of_prime_factors_of_a_number_matches_the_number(self):
+    for i in range(1, 1000):
+      result = self.prime_factors.factorize(i)
+      self.assertEqual(i, numpy.prod(result))
 
 if __name__ == '__main__':
   unittest.main()
--- src/prime_factors.py
+++ src/prime_factors.py
@@ -5,6 +5,7 @@ class PrimeFactors:
   def factorize(self, number):
     if number < 2:
       return []
+
     for divisor in [2] + list(range(3, floor(sqrt(number)), 2)):
       if number % divisor == 0:
         quotient = number // divisor
@@ -12,4 +13,5 @@ class PrimeFactors:
           return [quotient] + self.factorize(divisor)
         else:
           return [number]
+          
     return [number]
