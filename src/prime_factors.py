0f87910b7f6178257ccc4e9f1462c7d85b97d53c
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-04-11 14:42:26 +0100

    Refactor: Four has two twice as prime factor

--- src/prime_factors.py
+++ src/prime_factors.py
@@ -1,8 +1,15 @@
 
+from math import sqrt, floor
+
 class PrimeFactors:
   def factorize(self, number):
     if number < 2:
       return []
-    if number == 2 or number == 3:
-      return [number]
-    return [number / 2] + self.factorize(number / 2)
+    for divisor in [2] + list(range(3, floor(sqrt(number)), 2)):
+      if number % divisor == 0:
+        quotient = number // divisor
+        if quotient != 1:
+          return [quotient] + self.factorize(divisor)
+        else:
+          return [number]
+    return [number]
