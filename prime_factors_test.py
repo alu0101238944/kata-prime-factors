5118f3d2e2ae349f9c2acdf8155ef5083e4d3397
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-04-11 14:24:30 +0100

    Green: Two has only itself as prime factor

--- prime_factors_test.py
+++ prime_factors_test.py
@@ -9,5 +9,8 @@ class FizzBuzzTests(unittest.TestCase):
   def test_one_has_no_prime_factors(self):
     self.assertEqual(self.prime_factors.factorize(1), [])
 
+  def test_two_has_only_itself_as_prime_factor(self):
+    self.assertEqual(self.prime_factors.factorize(2), [2])
+
 if __name__ == '__main__':
   unittest.main()
--- src/prime_factors.py
+++ src/prime_factors.py
@@ -1,4 +1,4 @@
 
 class PrimeFactors:
   def factorize(self, number):
-    return []
+    return [] if number == 1 else [2]
