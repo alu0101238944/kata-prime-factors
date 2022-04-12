47531fab2cee338369314f400e3bd21f7e45d53a
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-04-12 16:46:29 +0100

    Negative numbers do not have prime factors

--- src/prime-factors.ts
+++ src/prime-factors.ts
@@ -1,6 +1,6 @@
 
 export class PrimeFactors {
   factorize(n: number) {
-
+    return [];
   };
 };
--- test/prime-factors-test.spec.ts
+++ test/prime-factors-test.spec.ts
@@ -2,3 +2,11 @@
 import {PrimeFactors} from '../src/prime-factors';
 
 const primeFactors = new PrimeFactors();
+
+describe('Base cases', () => {
+  test('Negative numbers do not have prime factors', () => {
+    expect(primeFactors.factorize(-1)).toEqual([]);
+    expect(primeFactors.factorize(-2)).toEqual([]);
+    expect(primeFactors.factorize(-3)).toEqual([]);
+  });
+});
