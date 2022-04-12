e34fd329b4e1fcf7145e36404f8d4766a8274a5b
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-04-12 17:48:25 +0100

    Refactoring main function

--- src/prime-factors.ts
+++ src/prime-factors.ts
@@ -2,16 +2,21 @@
 export class PrimeFactors {
   factorize(n: number) {
     const primeFactors = [];
-    
+
     if (n > 1) {
       const sqrtN = Math.floor(Math.sqrt(n));
-      let divisor = 2;
-      while (divisor <= sqrtN) {
+      // divisors = [1, 3, 5, 7, 9, 11, ..., (sqrtN | sqrtN - 1)]
+      const divisors = Array.from(Array(sqrtN).keys())
+                            .filter((e: number) => e % 2 === 1);
+      divisors.shift();     // Pop 1
+      divisors.unshift(2);  // Push 2
+
+      // divisor in range [2, 3, 5, 7, 9, 11, ..., (sqrtN | sqrtN - 1)]
+      for (const divisor of divisors) {
         while (n % divisor === 0) {
           n /= divisor;
           primeFactors.push(divisor);
         }
-        divisor++;
       }
       if (n > 1) {
         primeFactors.push(n);
