54f5e5449a793838d6c032b3a0dbd3033c66732c
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-04-15 14:03:32 +0100

    Red: Numbers smaller than 2 have no prime factors

--- prime_factors_test.py
+++ prime_factors_test.py
@@ -3,7 +3,9 @@ import unittest
 from src.prime_factors import PrimeFactors
 
 class PrimeFactorsTest(unittest.TestCase):
-  pass
+  def test_numbers_smaller_than_2_have_no_prime_factors(self):
+    for n in [-3, -2, -1, 0, 1]:
+      self.assertRaises(ValueError, PrimeFactors(n))
 
 if __name__ == "__main__":
   unittest.main()
