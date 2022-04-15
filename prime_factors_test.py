d0d0e5b6940253509d0eeab96c589147e2f30d72
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-04-15 14:41:50 +0100

    Red: First prime number only has itself as prime factor

--- prime_factors_test.py
+++ prime_factors_test.py
@@ -7,5 +7,8 @@ class PrimeFactorsTest(unittest.TestCase):
     for number in [-3, -2, -1, 0, 1]:
       self.assertRaises(ValueError, PrimeFactors, number)
 
+  def test_first_prime_number_only_has_itself_as_prime_factor(self):
+    self.assertEqual(PrimeFactors(2).get_prime_factors(), [2])
+
 if __name__ == "__main__":
   unittest.main()
