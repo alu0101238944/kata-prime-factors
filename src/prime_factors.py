ba9f8a0baa3275fea0d727bf9090c6c2d3add1f7
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-04-15 14:44:15 +0100

    Refactor: First prime number only has itself as prime factor

--- src/prime_factors.py
+++ src/prime_factors.py
@@ -3,8 +3,12 @@ class PrimeFactors():
   def __init__(self, number):
     if number > 1:
       self.number = number
+      self.needed_divisors = self.__compute_needed_divisors()
     else:
       raise ValueError('Numbers smaller than 2 have no prime factors')
 
-  def get_prime_factors(self):
+  def __compute_needed_divisors(self):
     return [self.number]
+
+  def get_prime_factors(self):
+    return self.needed_divisors
