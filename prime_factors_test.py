4b5734f376f7e5200393c6f9be783d29e29d7748
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-04-15 14:52:08 +0100

    Red: Powers of two has only twos as prime factors

--- prime_factors_test.py
+++ prime_factors_test.py
@@ -10,5 +10,12 @@ class PrimeFactorsTest(unittest.TestCase):
   def test_first_prime_number_only_has_itself_as_prime_factor(self):
     self.assertEqual(PrimeFactors(2).get_prime_factors(), [2])
 
+  def test_powers_of_two_has_only_twos_as_prime_factors(self):
+    for power in range(1, 11):
+      prime_factors_value = PrimeFactors(2 ** power).get_prime_factors()
+      expected = [2] * power
+
+      self.assertEqual(prime_factors_value, expected)
+
 if __name__ == "__main__":
   unittest.main()
